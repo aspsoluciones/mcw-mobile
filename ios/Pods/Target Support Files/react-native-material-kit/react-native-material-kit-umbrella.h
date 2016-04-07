58edad838c095a0d17630848ef999fc532fdedea
Epotignano <epotignano@gmail.com>
2016-04-07 11:57:28 -0300

    PodFile and more changes for xCode

---
+++ ios/Pods/Target Support Files/react-native-material-kit/react-native-material-kit-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "MKTouchable.h"
+#import "MKUtils.h"
+#import "TickView.h"
+#import "UIColor+MKColor.h"
+
+FOUNDATION_EXPORT double react_native_material_kitVersionNumber;
+FOUNDATION_EXPORT const unsigned char react_native_material_kitVersionString[];
+
